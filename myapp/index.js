e1c3526cf57b357bcc5de43485d8cadcd8f9e6f2
emil51z8 <emil51z8@edu.zealand.dk>
2023-10-27 10:59:45 +0200

    first commit

---
+++ myapp/index.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.get('/hest', (req, res) => {
+    res.send('<h1> Hello Hest! </h1>')
+  })
+
+  app.get('/api/hest', (req, res) => {
+    res.send('{hest: "hest", owner: "Emil", age: 12}')
+  })
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
